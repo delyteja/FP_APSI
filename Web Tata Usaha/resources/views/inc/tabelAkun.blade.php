c6f60105315b9cc7805e8e23058572cc22eae7b9
Nuuri <farzanurifan@gmail.com>
2017-11-19 19:10:40 +0700

    Web Tata Usaha

---
+++ Web Tata Usaha/resources/views/inc/tabelAkun.blade.php
@@ -0,0 +1,43 @@
+<table class="table table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Action</th>
+      <th scope="col">id</th>
+      <th scope="col">nama</th>
+      <th scope="col">bukti transfer</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>
+        <a onclick="berhasil()" href="#">approve</a>
+        <a onclick="berhasil()" href="#">reject</a>
+      </td>
+      <td>041</td>
+      <td>Farza Nurifan</td>
+      <td><a href="https://jokisealsby.files.wordpress.com/2016/09/s__2007162.jpg">image</a></td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>
+        <a onclick="berhasil()" href="#">approve</a>
+        <a onclick="berhasil()" href="#">reject</a>
+      </td>
+      <td>041</td>
+      <td>Farza Nurifan</td>
+      <td><a href="https://jokisealsby.files.wordpress.com/2016/09/s__2007162.jpg">image</a></td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>
+        <a onclick="berhasil()" href="#">approve</a>
+        <a onclick="berhasil()" href="#">reject</a>
+      </td>
+      <td>041</td>
+      <td>Farza Nurifan</td>
+      <td><a href="https://jokisealsby.files.wordpress.com/2016/09/s__2007162.jpg">image</a></td>
+    </tr>
+  </tbody>
+</table>
